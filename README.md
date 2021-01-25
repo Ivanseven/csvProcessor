aecfbead6384a5d4ba8e2c945d49e970d274e0a0
IvanY <sir.ivanyeong@gmail.com>
2021-01-25 14:17:05 +0800

    Add missing commit details

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Created on 8+ GMT 1:30pm, 25 Jan, 2021
-
+devEnv: Node v12.13.0
--- employee.js
+++ employee.js
@@ -1,2 +1,22 @@
 console.log("Hello! Good Day!")
 
+// input:
+let testInput = `First Name,Last Name,Annual Salary,Super Rate (%),Payment Start Date
+Andrew,Smith,60050,9%,01 March – 31 March
+Claire,Wong,120000,10%,01 March – 31 March`
+
+let employeesJson = [
+	["Andrew", "Smith", "60050", "9%", "01 March – 31 March"],
+	["Claire", "Wong", "120000", "10%","01 March – 31 March"]
+]
+
+function prepareCSV(csvString){
+	csvString.split(",")
+	console.log(csvString)
+}
+
+function getSalaryRates(employeesArray){
+
+}
+
+prepareCSV(testInput)
