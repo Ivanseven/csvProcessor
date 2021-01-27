0167e8be0b2a60d7a67bb603838f32c2c160c0e3
IvanY <sir.ivanyeong@gmail.com>
2021-01-27 19:05:22 +0800

    Replaced writeStream function to push csvString unto the stream with a given outputDir

--- writeStream.js
+++ writeStream.js
@@ -1,36 +1,67 @@
 const fs = require('fs')
-const salaryCSV = require('./parse.js')
+const salaryCSV = require('./parseCSV.js')
+
+module.exports = writeStream
+
+
+function writeStream (csvString, outputPath){
+	var outputPath = outputPath || './output.csv'
+	// This opens up the writeable stream to `output`
+	const writeStream = fs.createWriteStream(outputPath);
+
+	const { Readable } = require('stream'); 
+
+	const inStream = new Readable({
+	  read() {}
+	});
+
+	// inStream.push('ABCDEFGHIJKLM');
+	// inStream.push('NOPQRSTUVWXYZ');
+	inStream.push(csvString)
+	inStream.push(null); // No more data
+
+	// inStream.pipe(process.stdout);
+	inStream.pipe(writeStream)
+	console.log(`Your file was written to:\n ${outputPath}`)
+}
+
+
+
+
+
+
+
 
 // Stream version
-function openStream(inputPath, outputPath){
+function oldStream(inputPath, outputPath){
 
-var outputPath = outputPath || './output'
-var writeStream = fs.createWriteStream(outputPath);
-var readStream = fs.createReadStream(inputPath);
+	var outputPath = outputPath || './output'
 	// This opens up the writeable stream to `output`
-	var writeStream = fs.createWriteStream('./output');
+	var writeStream = fs.createWriteStream(outputPath);
+	var readStream = fs.createReadStream(inputPath);
 
 	readStream.on('open', function () {
-	// This just pipes the read stream to the response object (which goes to the client)
-	readStream.pipe(writeStream);
+		// This just pipes the read stream to the response object (which goes to the client)
+		readStream.pipe(writeStream);
 	});
 
 	// After all the data is saved, respond with a simple html form so they can post more data
 	readStream.on('end', function () {
-	// res.writeHead(200, {"content-type":"text/html"});
-	// res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
+		console.log("We have finished processing your CSV")
+		console.log(`Your output is saved at: ${true}`)
 	});
 
 	// This is here incase any errors occur
 	writeStream.on('error', function (err) {
-	console.log(err);
+		console.log(err);
 	});
+
 	readStream.on('error', function (err) {
-	console.log(err);
-	console.log("He!")
+		console.log("Failed to read file")
+		console.log(err);
 	});
 }
 
-// openStream("./dsa.csv")
+// writeStream("./dsa.csv")
 
-openStream("./testEmployeesCSV.csv")
+// writeStream("./testEmployeesCSV.csv")
